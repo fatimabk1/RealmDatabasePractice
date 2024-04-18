92802fc5c49a57be8342f0345ada217d9bdb969e
fatimabk1 <fatima.kahbi@gmail.com>
2024-04-18 09:40:50 -0400

    Initial Commit

---
+++ RealmProject/RealmProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmProjectApp.swift
+//  RealmProject
+//
+//  Created by Fatima Kahbi on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct RealmProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
